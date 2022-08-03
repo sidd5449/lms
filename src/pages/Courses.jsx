2ebe4a1ae1c4c53dfc13b85131e0d08cb70fbc2f
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2022-08-03 14:24:09 +0530

    added course

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
 
 const Courses = () => {
   return (
-    <div>Courses</div>
+    <>
+    
+        <Navbar />
+    
+    </>
   )
 }
 
--- src/pages/Dash.jsx
+++ src/pages/Dash.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
 
 const Dash = () => {
   return (
-    <div>Dash</div>
+    <>
+      <Navbar />
+    
+    </>
   )
 }
 
