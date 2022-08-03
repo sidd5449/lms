8a348ea0513c5095b38e1b45eeeb4fd95e9991a0
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2022-08-03 15:12:34 +0530

    configured dashboard

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar/Navbar';
+import Login from './Login';
 
 const Courses = () => {
+  
+  // const [token, settoken] = useState();
+
+  // if(!token){
+  //   return <Login settoken = {settoken} />
+  // }
+  
   return (
     <>
     
         <Navbar />
+        
+
     
     </>
   )
