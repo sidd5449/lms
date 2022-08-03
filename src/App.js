2ccbb6162c58f8c549d9cb217f86395f7d008b98
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2022-08-03 14:12:22 +0530

    added more schemas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import  { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Dash from './pages/Dash';
+import Login from './pages/Login';
+import Courses from './pages/Courses';
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element = {<HomePage />} />
         <Route path='dashboard' element = {<Dash />} />
+        <Route path='login' element = {<Login />} />
+        <Route path='courses' element = {<Courses />} />
       </Routes>
     </div>
   );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Hero.scss';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="app__hero">
+      <div className="app__hero-left">
+        <h1>An Effective LMS Solution</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+
+        <div className="button-group">
+          <a href="/login"><button>Get Started</button></a>
+          <a href="/courses" ><button id='secondary'>Courses</button></a>
+        </div>
+      </div>
+      <div className="app__hero-right"></div>
+    </div>
   )
 }
 
