c9af26d82c5391b7f05fbca630d2392def4acc20
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2022-08-12 22:51:56 +0530

    some major changes

--- src/pages/CourseInfo.jsx
+++ src/pages/CourseInfo.jsx
@@ -1,77 +1,123 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { client, getUrlFromId, urlFor } from '../client';
 import Navbar from '../components/Navbar/Navbar';
+import { FiDownload } from 'react-icons/fi';
 import './CourseInfo.scss'
 
 const CourseInfo = () => {
-    const docId = useParams();
-    const [subject, setsubject] = useState([]);
-    const [subArray, setsubArray] = useState([]);
-    const [notesArray, setnotesArray] = useState([]);
-  
-    // const apiUrl = `https://cyhyv1x0.api.sanity.io/v2022-02-01/data/doc/production/`;
-    // console.log(apiUrl);
-    
-    useEffect(() => {
-      const query = `*[_id == "${docId._id}"]`;
-      client.fetch(query)
-        .then((subjectDatabyID) =>{
-          setsubject(subjectDatabyID);
-        })
-    });
-    useEffect(() => {
-      subject.map((subjectItem) => {
-        const query = `*["${subjectItem.subjectName}" in tags]`;
-        client.fetch(query)
-          .then((notesData) =>{
-            setnotesArray(notesData);
-            console.log(notesArray);
-          })
+  const docId = useParams();
+  const [subject, setsubject] = useState([]);
+  const [subArray, setsubArray] = useState([]);
+  const [notesArray, setnotesArray] = useState([]);
+  const [submissions, setSubmissions] = useState([]);
+
+  // const apiUrl = `https://cyhyv1x0.api.sanity.io/v2022-02-01/data/doc/production/`;
+  // console.log(apiUrl);
+
+  useEffect(() => {
+    const query = `*[_id == "${docId._id}"]`;
+    client.fetch(query)
+      .then((subjectDatabyID) => {
+        setsubject(subjectDatabyID);
       })
-    });
+  });
+  useEffect(() => {
+    subject.map((subjectItem) => {
+      const query = `*["${subjectItem.subjectName}" in tags]`;
+      client.fetch(query)
+        .then((notesData) => {
+          setnotesArray(notesData);
+          console.log(notesArray);
+        })
+    })
+  });
+  useEffect(() => {
+    const query = '*[_type == "submissions"]';
+    client.fetch(query)
+      .then((submissionData) => {
+        setSubmissions(submissionData);
+      })
+  }, []);
 
-    
 
-    
-    
-    
 
-    
-    
-    
-  
-    return (
+
+
+
+
+
+
+
+
+
+  return (
     <div className="app__courseinfo">
-        
 
-        <Navbar />
-        <div className="app__courseinfo-course">
-            <div className="app__courseinfo-ourse-item">
-              {subject.map((subjectItem) => (
-                <div className="app__courseinfo-title">
-                  <p id='courseName'>{subjectItem.subjectName}</p>
-                  <p>Tutor: {subjectItem.subjectFac}</p>
-                </div>
-              ))}
-            </div>
-            <div className="app__courseinfo-coursenotes">
-              {notesArray.map((notesItem) => {
-                const fileLink = getUrlFromId(`${notesItem.notesFile.asset._ref}`);
-                return(
-                  <div className="app__note-item">
-                    <h3>{notesItem.fileName}</h3>
-                    <a href={fileLink} target="_blank">Download</a>
-                  </div>
-                )
-              })}
-            </div>
+
+      <Navbar />
+      <div className="app__courseinfo-wrapper">
+        <div className="app__courseinfo-course app__coursepage-taglines">
+          <div className="app__courseinfo-course-item">
+            {subject.map((subjectItem) => (
+              <div className="app__courseinfo-title">
+                <p id='welcome'>{subjectItem.subjectName}</p>
+                {/* <p>Tutor: {subjectItem.subjectFac}</p> */}
+              </div>
+            ))}
+          </div>
         </div>
-          
-        
+        <div className="app__courseinfo-content">
+          <div className="app__courseinfo-coursenotes">
+            <h2>Your Notes</h2>
+            {notesArray.map((notesItem) => {
+              const fileLink = getUrlFromId(`${notesItem.notesFile.asset._ref}`);
+              return (
+                <div className="app__note-item">
+                  <div className="app__note-item-desc">
+                    <div className="bullet"></div>
 
-          
+                    <div className="app__note-item-title">
+                      <h3>{notesItem.fileName}</h3>
+                      <p>Uploaded on {notesItem._createdAt}</p>
+                    </div>
+                  </div>
+                  <Link to={fileLink} target="_blank" className='downloadIcon'>
+                    <FiDownload
+                      size={20}
+                      color='var(--tertiary-color)'
+
+
+                    />
+
+                  </Link>
+
+
+                </div>
+              )
+            })}
+          </div>
+            <div className="app__upcoming">
+            <h2>Upcoming Submissions</h2>
+            {submissions.map((submission, index) => (
+              <div className="app__upcoming-sub">
+                <div className="app__upcoming-sub-title">
+                  <h3>{submission.title}</h3>
+                  <p>{submission.dueDate}</p>
+                </div>
+                <Link to='/' className='submit-btn'>
+                  <button>
+                    Submit Now
+                  </button>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+        
+      </div>
     </div>
+  
   )
 }
 
