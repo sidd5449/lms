3a66a4ee3913bb8d57b44c5e2011c81ae1ef8a95
Siddhesh Thakare <76845396+sidd5449@users.noreply.github.com>
2022-09-08 12:43:41 +0530

    Update fbConfig.js

--- src/utils/fbConfig.js
+++ src/utils/fbConfig.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {GoogleAuthProvider,getAuth,signInWithPopup,signInWithEmailAndPassword,createUserWithEmailAndPassword,sendPasswordResetEmail,signOut}from "firebase/auth";
 import {getFirestore,query,getDocs,collection,where,addDoc} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCjz3-n4kPS0erCNMlPiWdMGd329SXyo1g",
   authDomain: "educater-16289.firebaseapp.com",
@@ -18,7 +13,7 @@ const firebaseConfig = {
   measurementId: "G-9ZG5E85BBP"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
